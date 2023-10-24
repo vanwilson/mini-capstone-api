47ba96ba9b6be89d6bde49ff133310a22736e941
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-10-24 11:56:10 -0500

    added 5 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# product = Product.new(name: "Duck Magnet", price: 0.50, image_url: "TBD", desription: "magnet in shape of a duck")
+# product = Product.new(name: "Turtle Magnet", price: 0.50, image_url: "TBD", desription: "magnet in shape of a turtle")
+# product = Product.create(name: "Apple Magnet", price: 0.50, image_url: "TBD", desription: "magnet in shape of a Apple")
+# product = Product.create(name: "Star Magnet", price: 0.50, image_url: "TBD", desription: "magnet in shape of a Star")
+# product = Product.create(name: "Butterfly Magnet", price: 0.50, image_url: "TBD", desription: "magnet in shape of a Butterfly")
