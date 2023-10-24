2e25d0cdc52b65136883e4628e1875819b15dc8e
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-10-24 14:30:32 -0500

    create specific route using id paramater

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,28 +4,8 @@ class ProductsController < ApplicationController
     render :index
   end
 
-  def first_product
-    @product = Product.first
-    render :show
-  end
-
-  def second_product
-    @product = Product.second
-    render :show
-  end
-
-  def third_product
-    @product = Product.third
-    render :show
-  end
-
-  def fourth_product
-    @product = Product.fourth
-    render :show
-  end
-
-  def fifth_product
-    @product = Product.fifth
+  def one_product
+    @product = Product.find_by(id: params["id"])
     render :show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "all_products"
-  get "/first_product", controller: "products", action: "first_product"
-  get "/second_product", controller: "products", action: "second_product"
-  get "/third_product", controller: "products", action: "third_product"
-  get "/fourth_product", controller: "products", action: "fourth_product"
-  get "/fifth_product", controller: "products", action: "fifth_product"
+  get "/one_product/:id", controller: "products", action: "one_product"
 end
