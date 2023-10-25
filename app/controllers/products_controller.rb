33169122c79697743d1ebaed1d3ed17d1e5e8942
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-10-25 14:00:14 -0500

    create patch route for end user to edit data

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,4 +19,15 @@ class ProductsController < ApplicationController
     @product.save
     render :show
   end
+
+  def update
+    @product = Product.find_by(id: params["id"])
+    @product.update(
+      name: params["name"] || product.name,
+      price: params["price"] || product.price,
+      image_url: params["image_url"] || product.image_url,
+      description: params["description"] || product.description,
+    )
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/products", controller: "products", action: "all_products"
   get "/products/:id", controller: "products", action: "one_product"
   post "/products/new", controller: "products", action: "create"
+  patch "/products/:id" => "products#update"
 end
