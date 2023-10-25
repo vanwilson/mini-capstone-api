2ff37d5efe620025043fc5954ceb5c999546c4f3
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-10-25 12:21:34 -0500

    migrate db and added products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-# # product = Product.new(name: "Duck Magnet", price: 0.50, image_url: "TBD", description: "magnet in shape of a duck")
-# product.save
-# # product = Product.new(name: "Turtle Magnet", price: 0.50, image_url: "TBD", description: "magnet in shape of a turtle")
-# product.save
-# # product = Product.create(name: "Apple Magnet", price: 0.50, image_url: "TBD", description: "magnet in shape of a Apple")
-# product.save
-# # product = Product.create(name: "Star Magnet", price: 0.50, image_url: "TBD", description: "magnet in shape of a Star")
-# product.save
-# # product = Product.create(name: "Butterfly Magnet", price: 0.50, image_url: "TBD", description: "magnet in shape of a Butterfly")
-# product.save
+product = Product.new(name: "Duck Magnet", price: 0.50, image_url: "TBD", description: "magnet in shape of a duck")
+product.save
+product = Product.new(name: "Turtle Magnet", price: 0.50, image_url: "TBD", description: "magnet in shape of a turtle")
+product.save
+product = Product.create(name: "Apple Magnet", price: 0.50, image_url: "TBD", description: "magnet in shape of a Apple")
+product.save
+product = Product.create(name: "Star Magnet", price: 0.50, image_url: "TBD", description: "magnet in shape of a Star")
+product.save
+product = Product.create(name: "Butterfly Magnet", price: 0.50, image_url: "TBD", description: "magnet in shape of a Butterfly")
+product.save
