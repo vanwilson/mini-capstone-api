0655ec864ace94656fa85a835a1ccc8fee29cb04
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-10-25 14:28:00 -0500

    create delete path and method for end user

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,4 +30,10 @@ class ProductsController < ApplicationController
     )
     render :show
   end
+
+  def destroy
+    product = Product.find_by(id: params["id"])
+    product.destroy
+    render json: [message: "Product successfully deleted"]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/products/:id", controller: "products", action: "one_product"
   post "/products/new", controller: "products", action: "create"
   patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 end
