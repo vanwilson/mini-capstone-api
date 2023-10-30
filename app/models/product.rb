c3acb8f6c7f77ce59ecd38571e5ff3d340d15db3
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-10-30 12:02:47 -0500

    create validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  validates :name, length: { in: 2..20, message: "Keep name within 20 characters" }, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: true, greater_than: 0
+  validates :description, presence: true, length: { in: 10..500, message: "Keep description within 140 characters" }
+  validates :image_url, allow_blank: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: "must be a URL for GIF, JPG or PNG image." }
+
   def is_discounted?
     if price <= 10
       true
