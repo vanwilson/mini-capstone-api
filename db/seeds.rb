21e4bbc42263b64d6f0388e2fa38a18564518a1f
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-10-31 11:35:19 -0500

    add Supplier ID to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,10 @@
 # product.save
 # product = Product.create(name: "Butterfly Magnet", price: 0.50, image_url: "TBD", description: "magnet in shape of a Butterfly")
 # product.save
+
+# supplier = Supplier.new(name: "Magnets Magnets Magnets", email: "allthe@magnets.com", phone_number: "1234567890")
+# supplier.save
+# supplier = Supplier.new(name: "MagnetsRus", email: "weare@magnetsrus.com", phone_number: "1209877665")
+# supplier.save
+# supplier = Supplier.new(name: "Magnets Warehouse", email: "youwantem@wegotem.com", phone_number: "0987654321")
+# supplier.save
