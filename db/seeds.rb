adbea5b13e17f2a7d16cc0772a660e10608a624c
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-14 12:38:18 -0600

    unhashes bcrypt and adds user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
-# product = Product.new(name: "Duck Magnet", price: 0.50, description: "magnet in shape of a duck")
-# product.save
-# product = Product.new(name: "Turtle Magnet", price: 0.50, description: "magnet in shape of a turtle")
-# product.save
-# product = Product.create(name: "Apple Magnet", price: 0.50, description: "magnet in shape of a Apple")
-# product.save
-# product = Product.create(name: "Star Magnet", price: 0.50, description: "magnet in shape of a Star")
-# product.save
-# product = Product.create(name: "Butterfly Magnet", price: 0.50,, description: "magnet in shape of a Butterfly")
-# product.save
+product = Product.new(name: "Duck Magnet", price: 0.50, description: "magnet in shape of a duck")
+product.save
+product = Product.new(name: "Turtle Magnet", price: 0.50, description: "magnet in shape of a turtle")
+product.save
+product = Product.create(name: "Apple Magnet", price: 0.50, description: "magnet in shape of a Apple")
+product.save
+product = Product.create(name: "Star Magnet", price: 0.50, description: "magnet in shape of a Star")
+product.save
+product = Product.create(name: "Butterfly Magnet", price: 0.50, description: "magnet in shape of a Butterfly")
+product.save
 
-# supplier = Supplier.new(name: "Magnets Magnets Magnets", email: "allthe@magnets.com", phone_number: "1234567890")
-# supplier.save
-# supplier = Supplier.new(name: "MagnetsRus", email: "weare@magnetsrus.com", phone_number: "1209877665")
-# supplier.save
-# supplier = Supplier.new(name: "Magnets Warehouse", email: "youwantem@wegotem.com", phone_number: "0987654321")
-# supplier.save
+supplier = Supplier.new(name: "Magnets Magnets Magnets", email: "allthe@magnets.com", phone_number: "1234567890")
+supplier.save
+supplier = Supplier.new(name: "MagnetsRus", email: "weare@magnetsrus.com", phone_number: "1209877665")
+supplier.save
+supplier = Supplier.new(name: "Magnets Warehouse", email: "youwantem@wegotem.com", phone_number: "0987654321")
+supplier.save
 
 image = Image.new(url: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQoRS597YdSgT3duqp2XnOAgG7h3Z4G3ZUE8EaSnOqR1hKJEeD73soZ03FY7xAQv-5XNCVZEYTp89P42fYjeYczZQwktzCHv8hyZlUJxHTHkqCe-4twkjRM")
 image.save
