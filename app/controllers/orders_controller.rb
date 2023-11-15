89b3dcc48f25d4b8525e267cd73f29da39281b7d
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-15 13:10:22 -0600

    changes order to take in current_user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   def create
     @order = Order.new(
-      user_id: params[:user_id],
+      user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
       subtotal: params[:subtotal],
