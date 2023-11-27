c7c91fdc3bdac671d914a268fd193b7b6416d2d3
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-27 12:34:07 -0600

    adds categories method to products and products method to category

--- app/models/product.rb
+++ app/models/product.rb
@@ -22,10 +22,19 @@ class Product < ApplicationRecord
     price + tax
   end
 
+  has_many :images
   # def images
   #   Image.where(product_id: id)
   # end
-  has_many :images
+
   has_many :orders
+
   has_many :category_products
+
+  has_many :categories, through: :category_products
+  # def categories
+  #   category_products.map do |category_product|
+  #     category_product.category
+  #   end
+  # end
 end
