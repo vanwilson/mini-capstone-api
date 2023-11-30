81af23ba018cc67cab2290fe5fcc3853ac2a2d9d
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-30 13:08:49 -0600

    adds origin to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,6 +3,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
+    origins "mini-capstone.peterxjang.com", "localhost:5173"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
